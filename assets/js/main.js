1e6f09195b4b336cffae85a9c7e44b135f714b92
Manon Peawsathan <Manon@apples-MacBook-Air.local>
2019-06-21 22:14:30 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+<script>
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+</script>
